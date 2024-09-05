d5d1516316a1656f8750c1381988700c14fa5e53
Ahmed <vactum0@gmail.com>
2024-09-05 22:42:44 +0100

    fix file upload error

--- app.py
+++ app.py
@@ -2288,8 +2288,11 @@ elif st.session_state.step == 11:
             # Send email with attachments
             if st.session_state.files or local_file_path:
                 # Remove duplicates from st.session_state.files
-                st.session_state.files = list(set(st.session_state.files))
-                
+                # Remove duplicates while preserving order
+                seen = set()
+                st.session_state.files = [file for file in st.session_state.files if not (file in seen or seen.add(file))]
+
+
                 send_email_with_attachments(sender_email, sender_password, receiver_email, subject, body, st.session_state.files, local_file_path)
                 st.success("Submission Finished!")
                 st.session_state.submission_done = True
